f2a03f1d168e330dd8359df4e2e22a627c9f2be4
LaZeAsh <ayushrgarg@gmail.com>
2022-10-15 13:06:40 -0400

    Make auth work

--- google_auth.py
+++ google_auth.py
@@ -25,17 +25,17 @@ def oAuth():
         else:
             return False
 
-    # # If there are no (valid) credentials available, let the user log in.
-    # if not creds or not creds.valid:
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             'credentials.json', SCOPES)
-    #         creds = flow.run_local_server(port=0)
-    #     # Save the credentials for the next run
-    #     with open('token.json', 'w') as token:
-    #         token.write(creds.to_json())
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        # with open('token.json', 'w') as token:
+        #     token.write(creds.to_json())
 
     # try:
     #     service = build('people', 'v1', credentials=creds)
@@ -56,6 +56,4 @@ def oAuth():
     # except HttpError as err:
     #     print(err)
 
-
-if __name__ == '__main__':
-    main()
+oAuth()
