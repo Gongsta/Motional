0d88b515e83e80d477b89ff0d9545b78cac0bdb8
Steven <gong.steven@hotmail.com>
2022-10-15 05:48:39 -0400

    add pong and flappy bird games

---
+++ games/pong/README.md
@@ -0,0 +1,2 @@
+# Pong
+Pong with Multiplayer support. Modified from [here](https://github.com/arpruss/pong-clone) to support multiplier.
--- helper.py
+++ helper.py
@@ -15,6 +15,16 @@ def compute_distance(curr, target):
 	# Compute the distance between two points
 	return np.sqrt((target.x-curr.x)**2 + (target.y-curr.y)**2 + (target.z - curr.z)**2)
 
+def compute_relative_distance(curr, target, curr_reference, target_reference):
+	"""
+	This is in the case for face, where we need to first compute the relative distance.
+	"""
+	x = (target.x - target_reference.x) - (curr.x - curr_reference.x)
+	y = (target.y - target_reference.y) - (curr.y - curr_reference.y)
+	z = (target.z - target_reference.z) - (curr.z - curr_reference.z)
+
+	return np.sqrt(x**2 + y**2 + z**2)
+
 def store_new_pose(landmark, key, stored_keys):
 	# Store the new mapping of a key to a pose in the stored_keys dictionary
 	stored_keys[key] = landmark
@@ -43,16 +53,22 @@ def search_face_pose(landmark, stored_keys):
 	"""
 	Search if this landmark is a face pose we should be detecting
 	
-	Expects
 	
+	145 - 159 -> left eye (opening / closing)
+	374 - 386 -> right eye (opening / closing)
+	13 - 14 -> Mouth (opening closing)
+	
+	This doesn't work super well, we might actually need haars-cascade.
+
 	"""
-	max_dist = 0.02
+	max_dist = 0.04
 
 	for key in stored_keys:
 		works = True
-		for i in [4,8,12,16,20]:
-			print(compute_distance(stored_keys[key][i], landmark[i]))
-			if (compute_distance(stored_keys[key][i], landmark[i]) > max_dist):
+		for i in [[145, 159], [374, 386], [13, 14]]: #https://google.github.io/mediapipe/solutions/face_mesh.html
+			dist = compute_relative_distance(stored_keys[key][i[1]], landmark[i[1]], stored_keys[key][i[0]], landmark[i[0]])
+			print(dist)
+			if (dist > max_dist):
 				works = False
 				break
 		if works:
